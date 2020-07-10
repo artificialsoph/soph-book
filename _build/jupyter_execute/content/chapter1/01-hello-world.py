41301db3566b50063bed1645b3fa4cb1f99569eb
artificialsoph <s@soph.info>
2020-07-09 20:56:56 -0700

    update hello world

---
+++ _build/jupyter_execute/content/chapter1/01-hello-world.py
@@ -0,0 +1,15 @@
+# Hello World
+
+Let's get started learning how to code in python. Before anything else, I want you to learn how to get python to do *something*.
+
+
+We'll do this by creating a ["Hello World" program](https://en.wikipedia.org/wiki/%22Hello,_World!%22_program), a very simple program that prints the statement "Hello World". This can be done by typing `print("hello world!")` into a code block and then selecting the run button. I've gone ahead and done this, so all you need to do is select "run".
+
+# example below
+print("hello world!")
+
+Okay, now it's your turn. I've copied the same cell below but this time, I want you to modify the text between the two quote marks to say something other than "hello world". 
+
+#write your code below
+
+print("hello world!")
